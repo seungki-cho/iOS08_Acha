2e0f92b8f3c3f3cc9eeaaf206778484a3a28f11d
조승기 <beenzino@kookmin.ac.kr>
2022-12-02 00:54:41 +0900

    add: BadgeDTO

--- Acha/Acha/Data/BadgeDTO.swift
+++ Acha/Acha/Data/BadgeDTO.swift
@@ -10,6 +10,20 @@ import Foundation
 struct BadgeDTO: Codable {
     let id: Int
     let name: String
-    let image: String
+    let imageURL: String
     let isHidden: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case imageURL = "image"
+        case isHidden
+    }
+
+    func toDomain() -> Badge {
+        return Badge(id: id,
+                     name: name,
+                     imageURL: imageURL,
+                     isHidden: isHidden)
+    }
 }
